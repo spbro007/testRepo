90a839ae6df2acd1a1e8d39dcd28ebbee122bc9a
spbro007 <74732784+spbro007@users.noreply.github.com>
2021-06-07 03:50:23 +0530

    tasting
    toasting

---
+++ test.js
@@ -0,0 +1,7 @@
+var h1 = document.createElement('h1');
+h1.innerText = 'Hello World!';
+var h2 = document.createElement('h2');
+h2.innerText = 'Kaise ho aap ?';
+document.body.append(h1);
+document.body.append(h2);
+
