54146d2a5ada0885730f0afe1fcdb60d094b96eb
spbro007 <74732784+spbro007@users.noreply.github.com>
2021-06-07 17:51:46 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,2 @@
-var h1 = document.createElement('h1');
-h1.innerText = 'Hello World!';
-var h2 = document.createElement('h2');
-h2.innerText = 'Kaise ho aap ?';
-document.body.append(h1);
-document.body.append(h2);
+var h1=document.createElement("h1");h1.innerText="Hello World!";var h2=document.createElement("h2");h2.innerText="Kaise ho aap ?",document.body.append(h1),document.body.append(h2);
 
